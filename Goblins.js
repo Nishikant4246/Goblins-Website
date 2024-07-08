aea217371b542e7259937066f3ba7f102ceba459
Nishikant4246 <nishikantkshirsagar22@gmail.com>
2024-07-08 23:55:38 +0530

    Updated by N

--- Goblins.js
+++ Goblins.js
@@ -5,3 +5,14 @@ menu.onclick = () =>{
   menu.classList.toggle('bx-x');
   navlist.classList.toggle('open');
 };
+
+const sr = ScrollRevel ({
+  distance: '65px',
+  duration: 1600,
+  delay: 450,
+  reset: true
+});
+
+sr.reveal('.hero-text', {delay:200, origin:'top'});
+sr.reveal('.hero-img', {delay:450, origin:'top'});
+sr.reveal('.icons', {delay:500, origin:'left'});
